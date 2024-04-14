eef437f08d27999eb21edc95e0537d596d96da22
umaisayan <armughanrehman2018@gmail.com>
2024-04-13 22:58:23 -0700

    complete

---
+++ script.js
@@ -0,0 +1,70 @@
+// first assigment
+
+// 1 true
+// 2 false
+// 3 false
+// 4 true
+// 5 false
+// 6 true
+// 7 true
+// 8 true
+// 9 false
+// 10 false
+// 11 true
+// 12 false
+// 13 false
+// 14 false
+// 15 false
+// 16 false
+// 17 true
+// 18 false
+// 19 true
+// 20 false
+
+
+
+// second assigment
+
+// 1 true
+// 2 false
+// 3 true
+// 4 true
+// 5 false
+// 6 true
+// 7 false
+// 8 false
+// 9 false
+// 10 true
+// 11 true
+// 12 false
+// 13 false
+// 14 false
+// 15 false
+// 16 false
+// 17 true
+// 18 false
+// 19 true
+
+
+// third assigment
+
+// 1 true
+// 2 true
+// 3 false
+// 4 true
+// 5 false
+// 6 true
+// 7 false
+// 8 false
+// 9 true
+// 10 true
+// 11 true
+// 12 false
+// 13 is ka ans nhi ha koi
+// 14 true
+// 15 false
+// 16 is ka ans nhi ha koi
+// 17 false
+// 18 false
+// 19 true
+// 20 true
